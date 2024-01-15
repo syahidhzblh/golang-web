e2807d19986001e7f73bb33bed11c56f13e7abda
syahid <syahidhizbullah17@gmail.com>
2024-01-15 15:39:28 +0700

    Add files via upload

---
+++ serve_file_test.go
@@ -0,0 +1,48 @@
+package golang_web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		http.ServeFile(writer, request, "./resources/test.html")
+	} else {
+		http.ServeFile(writer, request, "./resources/notfound.html")
+	}
+}
+
+func TestServeFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	server.ListenAndServe()
+}
+
+//go:embed resources/test.html
+var resourceTest string
+
+//go:embed resources/notfound.html
+var resourceNotFound string
+
+func ServeFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprint(writer, resourceTest)
+	} else {
+		fmt.Fprint(writer, resourceNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	server.ListenAndServe()
+}
